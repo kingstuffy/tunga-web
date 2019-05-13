b913623aeb9ea0f6cd7882c36fde2937c01a8201
Kazeem Olanipekun <kezyolanipekun@gmail.com>
2019-05-14 00:58:04 +0100

    added Developer profile

---
+++ src/latest-landing/home/DevProfile/DevProfile.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import './DevProfile.css';
+
+class DevProfile extends Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <div className="DevProfile">
+    
+      </div>
+    )
+  }
+}
+DevProfile.propTypes = {
+}
+
+export default DevProfile;
