497c769c2381e1266fa456832c5e6f78f76cc12d
Kazeem Olanipekun <kezyolanipekun@gmail.com>
2019-05-14 00:57:28 +0100

    added blog page

---
+++ src/latest-landing/home/BlogPage/BlogPage.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./BlogPage.scss";
+
+class BlogPage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="BlogPage">
+
+      </div>
+    );
+  }
+}
+
+BlogPage.propTypes = {};
+
+export default BlogPage;
