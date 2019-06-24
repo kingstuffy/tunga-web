6ad282e2d73d3cc49f07a63caaaf6d610dbc2014
david <tdsemakula@gmail.com>
2019-06-24 12:03:55 +0300

    fix: invoice number display

--- src/components/dashboard/payments/PaymentList.jsx
+++ src/components/dashboard/payments/PaymentList.jsx
@@ -201,11 +201,11 @@ export default class PaymentList extends React.Component {
                                                     </div>
                                                 );
                                             })
-                                        ):(invoice.finalized || invoice.last_sent_at)(
+                                        ):(invoice.number && (invoice.finalized || invoice.last_sent_at))?(
                                             <a href={`${ENDPOINT_INVOICES}${invoice.id}/download/?format=pdf`} target="_blank">
                                                 {invoice.number}
                                             </a>
-                                        )}</td>
+                                        ):null}</td>
                                         {[PENDING_IN, PAID_IN].includes(filter)?(
                                             <td>
                                                 {invoice.total_amount === invoice.amount?(
