17d8961c1a1f9118b1a3584c2227ccdde23e1146
Olawale Malomo <warlay234@gmail.com>
2019-07-07 16:02:36 +0100

    Adjust case studies section

--- src/latest-landing/home/CaseStudies/CaseStudyCard/CaseStudyCard.js
+++ src/latest-landing/home/CaseStudies/CaseStudyCard/CaseStudyCard.js
@@ -11,18 +11,18 @@ import {
 } from "reactstrap";
 
 class CaseStudyCard extends Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    const { caseStudy } = this.props;
-    return (
-            <div className="CaseStudyCard w-100" style={{ marginTop: "-20px"}}>
+    render() {
+        const { caseStudy } = this.props;
+        return (
+            <div className="CaseStudyCard w-100" style={{ marginTop: "-20px" }}>
                 <Card className="border-0 pt-0 w-100">
-                    <CardBody className="pt-0">
-                        <img src={caseStudy.icon} alt={caseStudy.title} />
-                        <CardTitle className="text-blue size-12 mt-3">
+                    <CardBody className="pt-0 p-0">
+                        <img src={caseStudy.icon} alt={caseStudy.title}/>
+                        <CardTitle className="text-blue size-18 mt-3 font-weight-bold">
                             {caseStudy.title}
                         </CardTitle>
                         <Row>
@@ -34,10 +34,10 @@ class CaseStudyCard extends Component {
                                 xl={4}
                                 className="border-right-2"
                             >
-                                <CardTitle className="text-blue text-uppercase size-12 mt-3">
+                                <CardTitle className="text-blue text-uppercase size-14 mt-2 mb-1">
                                     Technology
                                 </CardTitle>
-                                <CardText className="size-10">
+                                <CardText className="size-16">
                                     {caseStudy.tech}
                                 </CardText>
                             </Col>
@@ -49,27 +49,27 @@ class CaseStudyCard extends Component {
                                 xl={4}
                                 className="border-right-2"
                             >
-                                <CardTitle className="text-blue text-uppercase size-16 mt-3">
+                                <CardTitle className="text-blue text-uppercase size-14 mt-2 mb-1">
                                     Lead Time
                                 </CardTitle>
-                                <CardText className="size-12">
+                                <CardText className="size-16">
                                     {caseStudy.leadTime}
                                 </CardText>
                             </Col>
                             <Col sm={12} md={4} lg={4} xs={12} xl={4}>
-                                <CardTitle className="text-blue text-uppercase size-16 mt-3">
+                                <CardTitle className="text-blue text-uppercase size-14 mt-2 mb-1">
                                     Production Time
                                 </CardTitle>
-                                <CardText className="size-12">
+                                <CardText className="size-16">
                                     {caseStudy.price}
                                 </CardText>
                             </Col>
                         </Row>
-                        <CardText className="mt-4 mb-3 size-12">
+                        <CardText className="mt-4 mb-4 size-16 font-italic">
                             {" "}
                             {caseStudy.description}
                         </CardText>
-                        <Media style={{marginTop: "-5px"}}>
+                        <Media className="mt-3">
                             <Media left href="#">
                                 <Media
                                     object
@@ -78,20 +78,22 @@ class CaseStudyCard extends Component {
                                     alt="Generic placeholder image"
                                 />
                             </Media>
-                            <Media body className="pl-2 size-12">
-                                <Media heading className="size-12 text-blue">
+                            <Media body className="pl-2 size-16">
+                                <Media heading className="size-16 text-blue mb-0">
                                     {caseStudy.user.name}
                                 </Media>
-                                {caseStudy.user.position +
+                                <Media className="size-14 font-italic">
+                                    {caseStudy.user.position +
                                     ", " +
                                     caseStudy.user.company}
+                                </Media>
                             </Media>
                         </Media>
                     </CardBody>
                 </Card>
             </div>
-    );
-  }
+        );
+    }
 }
 
 CaseStudyCard.propTypes = {};
