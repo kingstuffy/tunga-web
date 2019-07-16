50d72db4534acf3c644969d99fb9854d697b5e42
Olawale Malomo <warlay234@gmail.com>
2019-07-16 21:44:30 +0100

    Add search loader

--- src/latest-landing/home/OurTeam/TalentPool/TalentPool.js
+++ src/latest-landing/home/OurTeam/TalentPool/TalentPool.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { fetchTalentsRequest } from '../../../../services/talents/actions';
 import "./TalentPool.scss";
 import Carousel from "../../../shared/Carousel/Carousel";
+import Loader from "../../../shared/Loader/Loader";
 import Talent from "./Talent/Talent";
 import TalentSearch from "./TalentSearch/TalentSearch";
 
@@ -48,7 +49,7 @@ class TalentPool extends Component {
 
 
     render() {
-        const { talents } = this.props;
+        const { talents, is } = this.props;
         const talentsPerRow = parseInt(talents.length / 2, 10);
         const splitTalents = [
             talents.slice(0, talentsPerRow),
@@ -64,37 +65,45 @@ class TalentPool extends Component {
                 <h4 className="text-primary size-16 text-uppercase font-weight-bold">
                     TALENT POOL
                 </h4>
-                <div className="TalentPool__seatch-form">
+                <div className="TalentPool__search-form">
                     <TalentSearch/>
                 </div>
                 <div className="TalentPool__container">
-                    <Carousel
-                        pagination={pagination}
-                        float="float-right"
-                        color="text-primary"
-                    >
-                        <ul className="TalentPool__list" style={{ left: this.state.leftPosition }}>
-                            {splitTalents.map(
-                                (members, i) => (
-                                    <div
-                                        key={i}
-                                        className="TalentPool__row"
-                                    >
-                                        {
-                                            members.map(
-                                                (member, j) => (
-                                                    <div
-                                                        className="TalentPool__item"
-                                                        key={j}>
-                                                        <Talent talent={member}/>
-                                                    </div>
-                                                )
-                                            )}
-                                    </div>
-                                )
-                            )}
-                        </ul>
-                    </Carousel>
+                    {
+                        is.loading
+                            ?
+                            <div className="TalentPool__loader">
+                                <Loader/>
+                            </div>
+                            :
+                            <Carousel
+                                pagination={pagination}
+                                float="float-right"
+                                color="text-primary"
+                            >
+                                <ul className="TalentPool__list" style={{ left: this.state.leftPosition }}>
+                                    {splitTalents.map(
+                                        (members, i) => (
+                                            <div
+                                                key={i}
+                                                className="TalentPool__row"
+                                            >
+                                                {
+                                                    members.map(
+                                                        (member, j) => (
+                                                            <div
+                                                                className="TalentPool__item"
+                                                                key={j}>
+                                                                <Talent talent={member}/>
+                                                            </div>
+                                                        )
+                                                    )}
+                                            </div>
+                                        )
+                                    )}
+                                </ul>
+                            </Carousel>
+                    }
                 </div>
             </section>
         );
