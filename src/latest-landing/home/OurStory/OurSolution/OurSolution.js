533fb101a44f890813c4549f526b6a343863a726
Olawale Malomo <warlay234@gmail.com>
2019-07-21 18:52:47 +0100

    Adjust our story sections

--- src/latest-landing/home/OurStory/OurSolution/OurSolution.js
+++ src/latest-landing/home/OurStory/OurSolution/OurSolution.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "./OurSolution.scss";
 import solVideo from "../../../assets/videos/our-story/BG-ourstory.mp4";
+
 // import  solVideoMobile from "../../../assets/videos/our-story/BG-ourstory-mobile.m4v";
 
 class OurSolution extends Component {
@@ -12,49 +13,35 @@ class OurSolution extends Component {
     render() {
         return (
             <div className="OurSolution">
-                <div className="row">
-                    <div className="OurSolution__content col-md-12 col-lg-5  col-sm-12">
-                        <div className="text-primary size-14 font-weight-bold mb-2 mt-3">
-                            OUR SOLUTION
-                        </div>
-                        <div className="text-blue size-18 font-weight-bold mb-2">
-                            Link African software developers to international teams and projects
-                        </div>
-                        <div className="mb-4">
-                            We believe in the power of business to transform society. And probably the most attractive
-                            area for creating high skilled jobs and skills development for today’s ambitious youths is
-                            software development.
-                        </div>
-                        <div className="mb-4">
-                            As production methods from the open source community are increasingly
-                            being adopted in the industry, software is often already being produced in virtual teams. At
-                            the same time, there is a huge shortage of software developers around the world, resulting
-                            in an infinite demand for software development skills.
-                        </div>
-                        <div>
-                            So Tunga was set up as a platform to
-                            remove all barriers for these youths to participate in international software teams and
-                            projects.
-                        </div>
+                <div className="OurSolution__content">
+                    <div className="text-primary size-14 font-weight-bold mb-2 mt-3">
+                        OUR SOLUTION
                     </div>
-                    <div className="OurSolution__video col-lg-7 col-md-12 col-sm-12 container">
-                        <video
-                            className="OurSolution__video"
-                            controls
-                            // autoPlay
-                            // loop
-                        >
-                            <source
-                                src={solVideo}
-                                type="video/mp4"
-                            />
-                            {/* <source
-                                src={solVideoMobile}
-                                type="video/mp4"
-                                media="all and (max-width: 768px)"
-                            /> */}
-                        </video>
+                    <div className="text-blue size-18 font-weight-bold mb-2">
+                        Link African software developers to international teams and projects
                     </div>
+                    <div className="mb-4">
+                        We believe in the power of business to transform society. And probably the most attractive
+                        area for creating high skilled jobs and skills development for today’s ambitious youths is
+                        software development.
+                    </div>
+                    <div className="mb-4">
+                        As production methods from the open source community are increasingly
+                        being adopted in the industry, software is often already being produced in virtual teams. At
+                        the same time, there is a huge shortage of software developers around the world, resulting
+                        in an infinite demand for software development skills.
+                    </div>
+                    <div>
+                        So Tunga was set up as a platform to
+                        remove all barriers for these youths to participate in international software teams and
+                        projects.
+                    </div>
+                </div>
+                <div className="OurSolution__video">
+                    <iframe allowFullScreen="allowFullScreen"
+                            src="https://www.youtube.com/embed/RVVtyapBmuo?ecver=1&amp;iv_load_policy=1&amp;yt:stretch=16:9&amp;autohide=1&amp;color=red&amp;width=100"
+                            width="100%" height="100%" allowTransparency="true" frameBorder="0">
+                    </iframe>
                 </div>
             </div>
         );
