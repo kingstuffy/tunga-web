e7a4ee38aee38fa1ba886df53286bab8037c232b
Olawale Malomo <warlay234@gmail.com>
2019-07-28 19:32:26 +0100

    Add chat widget

--- src/latest-landing/home/Home.js
+++ src/latest-landing/home/Home.js
@@ -2,42 +2,37 @@ import React, { Component } from "react";
 import "./Home.scss";
 import { Route } from "react-router-dom";
 import IconButton from "../shared/core/IconButton";
+import ChatWidget from "./ChatWidget/ChatWidget";
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    return (
-      <div className="Home">
-        <div>
-          {this.props.childRoutes.map((route, i) => {
-            return (
-              <Route
-                exact={route.exact}
-                key={i}
-                render={props => (
-                  <route.component
-                    name={route.name}
-                    {...props}
-                  />
-                )}
-                path={route.path}
-              />
-            );
-          })}
-        </div>
-        <div className="position-fixed icon-chat-bottom bg-primary">
-          <IconButton
-            name="chat-alt"
-            size="md"
-            className="text-white"
-          />
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="Home">
+                <div>
+                    {this.props.childRoutes.map((route, i) => {
+                        return (
+                            <Route
+                                exact={route.exact}
+                                key={i}
+                                render={props => (
+                                    <route.component
+                                        name={route.name}
+                                        {...props}
+                                    />
+                                )}
+                                path={route.path}
+                            />
+                        );
+                    })}
+                </div>
+                <ChatWidget autoOpen={true}/>
+            </div>
+        );
+    }
 }
 
 Home.propTypes = {};
