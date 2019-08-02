c67b964177bf3443f20e14d5999186ea936f6ea5
david <tdsemakula@gmail.com>
2019-08-02 08:47:03 +0300

    fix: switch sandbox api url to https

--- src/actions/utils/api.js
+++ src/actions/utils/api.js
@@ -6,7 +6,7 @@ axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 axios.defaults.withCredentials = true;
 
 let BACKEND_PATH =
-    __BACKEND_ROOT_URL__ || (__PRODUCTION__ ? (/butterflyworks\.org/ig.test(window.location.hostname) ? 'https://tunga.io/' : '/') : 'http://sandbox.tunga.io/');
+    __BACKEND_ROOT_URL__ || (__PRODUCTION__ ? (/butterflyworks\.org/ig.test(window.location.hostname) ? 'https://tunga.io/' : '/') : 'https://sandbox.tunga.io/');
 let API_PATH = 'api/';
 let SOCIAL_LOGIN_PATH = 'accounts/social/';
 
