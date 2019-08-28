ed42214c3c465f8c66d9fc72476fc0b2915d4f8e
Olawale Malomo <warlay234@gmail.com>
2019-08-28 17:44:17 +0100

    Change footer phone number

--- src/latest-landing/layout/Footer/Footer.js
+++ src/latest-landing/layout/Footer/Footer.js
@@ -237,7 +237,7 @@ class Footer extends Component {
                                     </a>
                                 </p>
                                 <p>
-                                    <a href="tel:+31615955194">+31615955194</a>
+                                    <a href="tel:++31 20 220 2157">+31 20 220 2157</a>
                                 </p>
                             </div>
 
