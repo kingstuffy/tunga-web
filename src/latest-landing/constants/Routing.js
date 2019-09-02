a850c26e8535ca8b8d53598168aca8b0f2446fe0
Olawale Malomo <warlay234@gmail.com>
2019-09-02 07:15:37 +0100

    Add login to dashbpard nav

--- src/latest-landing/constants/Routing.js
+++ src/latest-landing/constants/Routing.js
@@ -4,11 +4,12 @@
  */
 
 export default {
-  home: { path: "/", name: "Tunga" },
-  ourTeam: { path: "/our-team", name: "Our Team" },
-  ourStory: { path: "/our-story", name: "Our Story" },
-  news: { path: "/news", name: "News" },
-  blog: { path: "/blog", name: "Blog", loc: 'blog' },
-  login: { path: "/login", name: "Log in", loc: 'login' },
-  devProfile: { path: "/dev-profile/:id", basePath: "/dev-profile", name: "Developer Profile" },
+    home: { path: "/", name: "Tunga" },
+    ourTeam: { path: "/our-team", name: "Our Team" },
+    ourStory: { path: "/our-story", name: "Our Story" },
+    news: { path: "/news", name: "News" },
+    blog: { path: "/blog", name: "Blog", loc: 'blog' },
+    login: { path: "/login", name: "Log in", loc: 'login' },
+    devProfile: { path: "/dev-profile/:id", basePath: "/dev-profile", name: "Developer Profile" },
+    dashboard: { path: "/dashboard", name: "Log in", loc: 'dashboard' },
 };
