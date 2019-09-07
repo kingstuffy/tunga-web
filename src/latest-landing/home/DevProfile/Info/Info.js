4905b81f4097e2e4440622ba4359346b8531b8d2
Olawale Malomo <warlay234@gmail.com>
2019-09-07 21:58:36 +0100

    Make bio expadanble based on content

--- src/latest-landing/home/DevProfile/Info/Info.js
+++ src/latest-landing/home/DevProfile/Info/Info.js
@@ -7,36 +7,70 @@ import { Button } from "../../../shared/Form/Form";
 import { openCalendlyWidget } from "../../../../components/utils/calendly";
 
 
+const ShortBio = (({ bio, onBioOpen }) => {
+    const splitBio = bio.substring(0, 290).split(' ');
+    const shortBio = splitBio.slice(0, splitBio.length - 1).join(' ');
+
+    return (
+        <div>
+            {shortBio} <a className="Info__bio-link" onClick={onBioOpen}>...Read more</a>
+        </div>
+    );
+});
+
+
 class Info extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            skills: [
-                "HTML/CSS",
-                "JavaScript",
-                "GraphQL",
-                "Node.js",
-                "React Native"
-            ]
+            isBioOpen: false
         };
+
+        this.onBioToggle = this.onBioToggle.bind(this);
     }
 
+
+    onBioToggle() {
+        this.setState({ isBioOpen: !this.state.isBioOpen });
+    }
 
     render() {
         const { talent } = this.props;
 
         return (
             <div className="Info">
-                <img src={talent.image} className="Info__img mb-3"/>
+                {
+                    talent.image
+                    &&
+                    <img src={talent.image} className="Info__img mb-3"/>
+                }
                 <div className="size-13 text-primary mb-1">
                     {talent.display_name}
                 </div>
                 <div className="size-12 mb-3">
                     {talent.profile.city}, {talent.profile.country_name}
                 </div>
-                <div className="size-13 mb-4">
-                    {talent.profile.bio}
+                <div className="Info__bio-box">
+                    {
+                        this.state.isBioOpen
+                        &&
+                        <div className="Info__bio-full">
+                            {talent.profile.bio}
+                            <div>
+                                <a className="Info__bio-link" onClick={this.onBioToggle}>Close bio</a>
+                            </div>
+                        </div>
+                    }
+                    <div className="size-13 mb-4">
+                        {
+                            talent.profile.bio.length < 290
+                                ?
+                                talent.profile.bio
+                                :
+                                <ShortBio bio={talent.profile.bio} onBioOpen={this.onBioToggle}/>
+                        }
+                    </div>
                 </div>
                 <ul className="Info__skills mb-3">
                     {
