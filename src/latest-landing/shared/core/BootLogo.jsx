8db575d0f3f83aa5a6ddc9ace4ae75462a02d5ae
Remo Samuel Paul <remosam42@gmail.com>
2019-09-17 12:55:02 +0300

    added logo with new color

--- src/latest-landing/shared/core/BootLogo.jsx
+++ src/latest-landing/shared/core/BootLogo.jsx
@@ -5,7 +5,7 @@ const BootLogo = ({}) => {
   return (
         <div className="boot-logo">
             <div>
-                <img src={require("../../../assets/images/logo.png")} />
+                <img src={require("../../../assets/images/logo_new_color.png")} />
             </div>
             <Progress />
         </div>
