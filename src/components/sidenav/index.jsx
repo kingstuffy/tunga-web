07296797cb02994b370ec43201db8ae3e39b4901
Olawale Malomo <warlay234@gmail.com>
2019-11-05 10:51:02 +0100

    Refactor side bar

--- src/components/sidenav/index.jsx
+++ src/components/sidenav/index.jsx
@@ -9,74 +9,26 @@ class SideNav extends React.Component {
         super(props)
         this.hideLabelsTimer = null;
         this.showActiveLabelTimer = null;
-        this.observer = null;
-        this.observed = [];
-        this.triggerRender = true;
         this.state = {
             pages: this.props.pages
-        }
-
-        this.addObservers = this.addObservers.bind(this);
+        };
     }
+
 
     componentDidMount() {
-        this.observer = new IntersectionObserver(this.observerCallback, { threshold: 0.7 });
-        setTimeout(this.addObservers, 200);
+        this.showLabel();
     }
+
 
     componentWillUnmount() {
         clearTimeout(this.addObservers);
         clearTimeout(this.hideLabelsTimer);
+        clearTimeout(this.showActiveLabelTimer);
     }
 
-    addObservers() {
-        this.addObserverToTargets();
-        this.showLabel();
-    }
-
-    observerCallback = (entries, _observer) => {
-        const activeTargets = entries.map((entry) => {
-            if (entry.intersectionRatio > 0) {
-                return entry.target.id;
-            }
-            return entry.target
-        });
-
-        if (activeTargets.length !== this.props.pages.length) {
-            const activeHash = activeTargets[0];
-            const page = this.state.pages.find(obj => {
-                return obj.hash === activeHash
-            })
-            this.observed = [...this.observed, page]
-
-            if (this.triggerRender) {
-                this.triggerRender = false;
-                this.showLabel();
-            }
-
-            this.addObserverToTargets();
-        }
-
-    }
-
-    addObserverToTargets = () => {
-        const pages = this.state.pages.map((anc) => {
-            const target = document.getElementById(anc.hash);
-
-            if (target && !anc.isObserved) {
-                this.observer.observe(target);
-                return { ...anc, isObserved: true }
-            }
-
-            return anc;
-        });
-        this.setState({ pages })
-    }
 
     showLabel = () => {
         this.showActiveLabelTimer = setTimeout(() => {
-            this.triggerRender = true;
-            const active = this.observed[this.observed.length - 1]
             const pages = this.state.pages.map((obj, page) => {
                 return page === this.props.currentPage
                     ? { ...obj, isActive: true, isActiveBar: true } : { ...obj, isActive: false, isActiveBar: false };
@@ -85,6 +37,14 @@ class SideNav extends React.Component {
             this.hideLabels();
         }, 200);
     }
+
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.currentPage !== this.props.currentPage) {
+            this.showLabel();
+        }
+    }
+
 
     hideLabels() {
         this.hideLabelsTimer = setTimeout(() => {
@@ -101,6 +61,7 @@ class SideNav extends React.Component {
         event.preventDefault();
     }
 
+
     render() {
         const theme = this.state.pages[this.props.currentPage].theme;
         return (
@@ -110,13 +71,13 @@ class SideNav extends React.Component {
                         this.state.pages.map((anc, index) => (
                             <li className="side-bar-item" key={kebabCase(anc.title)}>
                                 <div className={`side-bar-tab
-                                ${this.props.currentPage === index ? "side-bar-tab-active"
+                                ${anc.isActiveBar ? "side-bar-tab-active"
                                     : "side-bar-tab-hidden"
                                     }`}>
                                 </div>
                                 <a style={{ width: `${anc.title.length * 8}px` }}
                                    className={`side-bar-label font-weight-bold
-                ${anc.isActive ? "side-bar-label-active"
+                                    ${anc.isActive ? "side-bar-label-active"
                                        : "side-bar-label-hidden"}`}
                                    href={`#${kebabCase(anc.title)}`}
                                    data-value={kebabCase(anc.title)}
