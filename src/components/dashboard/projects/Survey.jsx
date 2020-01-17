0ca479efb4479e5b168678cb581eba2f6b7d879a
Olawale Malomo <warlay234@gmail.com>
2020-01-17 12:41:58 +0100

    Replace project deadline with due at

--- src/components/dashboard/projects/Survey.jsx
+++ src/components/dashboard/projects/Survey.jsx
@@ -140,7 +140,7 @@ class Survey extends React.Component {
                         Client survey
                     </div>
                     <div>
-                        Due date: {moment.utc(project.deadline).local().format('lll')}
+                        Due date: {moment.utc(project.due_at).local().format('lll')}
                     </div>
                 </div>
 
